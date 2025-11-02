8682b5b453b9ef6842eefffdaf6a62bd09752a0f
rogerjove012005 <rogerjove012005@gmail.com>
2025-11-02 18:09:17 +0100

    Feat: date cleaning

--- Data/main.py
+++ Data/main.py
@@ -10,12 +10,21 @@ try:
     print("Primeras filas:\n", df.head(), "\n")
     print("Resumen estadístico:\n", df.describe(), "\n")
 
+    # Limpieza y preparación básica 
+    df['fecha'] = pd.to_datetime(df['fecha'], errors='coerce')
+    df = df.dropna(subset=['fecha'])  # elimina filas sin fecha válida
+    df['mes'] = df['fecha'].dt.month
+    df['dia_semana'] = df['fecha'].dt.day_name()
+
+    print("\nDatos después de limpieza y preparación:\n")
+    print(df.head())
+
     print("Promedio de unidades por producto:")
     print(df.groupby("producto")["unidades"].mean())
 
 except FileNotFoundError:
-    print(f"❌ No se encontró el archivo: {ruta}")
+    print(f" No se encontró el archivo: {ruta}")
 except pd.errors.EmptyDataError:
-    print("⚠️ El archivo está vacío o corrupto.")
+    print(" El archivo está vacío o corrupto.")
 except Exception as e:
-    print(f"⚠️ Ocurrió un error inesperado: {e}")
+    print(f" Ocurrió un error inesperado: {e}")
