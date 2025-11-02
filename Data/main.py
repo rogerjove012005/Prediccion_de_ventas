64d4681e6478fe0b8314fc505d3aa7d698b51d34
rogerjove012005 <rogerjove012005@gmail.com>
2025-11-02 17:23:02 +0100

    Feat: CSV reading

---
+++ Data/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\roger\Desktop\Python\Leer\Data\ventas.csv")
+print(df.head())
+print(df.describe())
--- main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('ventas.csv')
-print(df.head())
-print(df.describe())
