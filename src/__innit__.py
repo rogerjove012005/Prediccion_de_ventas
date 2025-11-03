a50f17f1d5959acaa5f7ebdfbc4b901bb3ff7c45
rogerjove012005 <rogerjove012005@gmail.com>
2025-11-03 21:37:45 +0100

    feat: init.py

---
+++ src/__innit__.py
@@ -0,0 +1,10 @@
+# src/__init__.py
+
+# Importar funciones comunes (opcional)
+from .features import crear_features
+
+# Definir versión del paquete (buena práctica)
+__version__ = "1.0.0"
+
+# Mensaje opcional al importar el paquete
+print("📦 Paquete 'src' cargado correctamente.")
